381992fb37e8b33e132fe290541f9e159df72ded
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-05-06 09:27:58 +0000

    fix: ZSH_CUSTOM, DOTFILES paths

--- install.sh
+++ install.sh
@@ -3,16 +3,16 @@ aliases() {
     echo "                  Import zsh aliases                       "
     echo "-----------------------------------------------------------"
     cd $HOME/dotfiles
-    cp ./aliases.zsh > $ZSH_CUSTOM/custom/
+    cp ./aliases.zsh > $ZSH/custom/
 }
 
 spaceship-prompt() {
    echo "==========================================================="
    echo "                Install spaceship prompt                   "
    echo "-----------------------------------------------------------"
-   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-   ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-   echo 'export ZSH_THEME="spaceship"' > $ZSH_CUSTOM/custom/prompt.zsh
+   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+   ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+   echo 'export ZSH_THEME="spaceship"' > $ZSH/custom/prompt.zsh
 }
 
 aliases
