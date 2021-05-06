4addaa082c5d7f027b090fbbb2d1d064eae4098b
Vladimir Dementyev <dementiev.vm@gmail.com>
2021-05-06 09:52:46 +0000

    fixup! fix: ZSH_CUSTOM -> ZSH

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
+DOTFILES="/workspaces/.codespaces/.persistedshare/dotfiles"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
 aliases() {
     echo "==========================================================="
     echo "                  Import zsh aliases                       "
     echo "-----------------------------------------------------------"
-    cd $HOME/dotfiles
-    cp ./aliases.zsh > $ZSH/custom/
+    cd $DOTFILES
+    cp ./aliases.zsh $ZSH_CUSTOM/
 }
 
 spaceship-prompt() {
    echo "==========================================================="
    echo "                Install spaceship prompt                   "
    echo "-----------------------------------------------------------"
-   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
-   ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
-   echo 'export ZSH_THEME="spaceship"' > $ZSH/custom/prompt.zsh
+   git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+   ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+   echo 'export ZSH_THEME="spaceship"' > $ZSH_CUSTOM/prompt.zsh
 }
 
 aliases
